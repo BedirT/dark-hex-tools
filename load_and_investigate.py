cec8e9bc904816d3fc14f3c1d2dc4c56329cf1f6
BedirT <tapkan@ualberta.ca>
2021-05-13 18:07:33 -0600

    4x3 pONE results added

--- load_and_investigate.py
+++ load_and_investigate.py
@@ -1,13 +1,46 @@
 # loads pickle data on given path
 import pickle
+import pandas as pd
+import numpy as np
 
-file_name = 'default_file.pkl'
+file_name = 'Exp_Results/4x3_pone_dp.pkl'
 
 with open(file_name, 'rb') as f:
     a = pickle.load(f)
 
-for i, e in enumerate(a):
-    for j, h in enumerate(e):
-        for x in h:
-            if h[x] != '=':
-                print(x, h[x], '| h=', j, ' | e=', i)
+ct_tot = [[0 for _ in range(7)] for _ in range(12)]
+ct_b = [[0 for _ in range(7)] for _ in range(12)]
+
+all_total = 0
+new_data_b = []
+new_data_tot = []
+y_size=range(len(a))
+x_size=range(len(a[0]))
+round_tot_b_h = [0] * len(a[0])
+for e in y_size:
+    round_tot_b = 0
+    for h in x_size:
+        tot_b = len([x for x in a[e][h] if a[e][h][x] != '='])
+        tot = len([x for x in a[e][h]])
+        new_data_b.append(tot_b)
+        new_data_tot.append(tot)
+        round_tot_b += tot_b
+        round_tot_b_h[h] += tot_b
+    new_data_b.append(round_tot_b)
+    all_total += round_tot_b
+round_tot_b_h.append(all_total)
+new_data_b.extend(round_tot_b_h)
+
+df = pd.DataFrame(np.array(new_data_b).reshape(len(a)+1, len(a[0])+1),
+                  index=[*y_size, 'TOT'], columns=[*x_size, 'TOT'])
+
+import seaborn as sns
+cm = sns.light_palette("blue", as_cmap=True)
+s = df.style.background_gradient(cmap=cm)
+s
+
+# inpe = 0
+# while inpe != -1:
+#     inpe, inph = map(int, input('Check for number of e and h: (input as - e h): ').strip().split(' '))
+#     print('Number of Black win positions: ', ct_b[inpe][inph])
+#     print('Number of total legal positions: ', ct_tot[inpe][inph])
