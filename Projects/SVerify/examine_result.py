3450460eaa3abf85da6332c88b45e07760429921
BedirT <tapkan@ualberta.ca>
2021-11-19 12:59:39 -0700

    examine results fixed

--- Projects/SVerify/examine_result.py
+++ Projects/SVerify/examine_result.py
@@ -5,9 +5,17 @@ Uses opp_info.pkl and info_states corresponding to the game.
 Presents a possibility for the examiner to select a state to examine.
 '''
 import pickle
-from Projects.SVerify.run import play_action
+
+from humanfriendly.terminal import ANSI_SHOW_CURSOR
+from Projects.SVerify.run import calculate_turn, play_action
 from strategy_data import strategies
 from Projects.base.game.hex import pieces, multiBoard_print
+import logging
+
+import coloredlogs
+
+log = logging.getLogger(__name__)
+coloredlogs.install(level='DEBUG')  # Change this to DEBUG to see more info.
 
 def choose_strategy():
     '''
@@ -25,42 +33,46 @@ def choose_strategy():
     choice = int(input('Enter your choice: '))
     return arr[choice]
 
-def display_options(boards, player_strategy, opp_strategy, p, turn):
+def display_options(game_state, turn):
     '''
     Displays the moves and probabilities that were chosen.
     
     turn: 0 for player, 1 for opponent
     '''
-    if turn == 0:
-        # print('Player\'s turn')
-        opts = player_strategy[boards[p]]
+    log.debug('Displaying options for turn: {}'.format(turn))
+    log.debug('Board State: {}'.format(game_state['board']))
+    if turn == game_state['player_order']:
+        opts = game_state['player_strategy'][game_state['boards'][game_state['player']]]
         for i, (a, p) in enumerate(opts):
             print('{}. {} - {}'.format(i, a, p))
     else:
-        opts = opp_strategy[boards[pieces.kBlack] + boards[pieces.kWhite]]
+        try:
+            opts = game_state['opponent_strategy'][game_state['boards'][game_state['opponent']]]
+        except KeyError:
+            log.critical('Opponent strategy missing board state: {}'.format(game_state['boards'][game_state['opponent']]))
+            exit()
+        if opts == -1:
+            # Opponent loses no matter what
+            print('Opponent loses no matter what')
+            return -1, -1
+        action_choices = []
         for i, a in enumerate(opts):
-            p = 1 / len(opts)
+            if a > -1:
+                action_choices.append((i, a))
+        try:
+            assert len(action_choices) > 0
+        except AssertionError:
+            log.critical('Opponent strategy is empty for board state: {}'.format(game_state['boards'][game_state['opponent']]))
+            exit()
+        for i, (a, p) in enumerate(action_choices):
             print('{}. {} - {}'.format(i, a, p))
     choice = int(input('Enter your choice: '))
-    if choice == -1:
-        return None
-    
-    return opts[choice] if turn == 0 else (opts[choice], 1 / len(opts))
+    return opts[choice] if turn == game_state['player_order'] else action_choices[choice]
 
 def main():
-    opp_strategy = pickle.load(open('Projects/SVerify/opp_info2.pkl', 'rb'))
+    opp_strategy = pickle.load(open('opp_info.pkl', 'rb'))
     game = choose_strategy()
-    player_strategy = game['strategy']
-    
-    # set up the board
-    num_rows = game['num_rows']
-    num_cols = game['num_cols']
-    
-    player_color = game['player']
-    opp_color = pieces.kWhite if game['player'] == pieces.kBlack else pieces.kBlack
-    player_order = game['player_order']
-    first_player = game['first_player']
-    
+    # set up the game
     game_state = {
         'board': game['board']
             if 'board' in game 
@@ -75,30 +87,39 @@ def main():
                 if 'boards' in game
                 else pieces.kEmpty * (game['num_rows'] * game['num_cols'])
         },
-        'num_rows': num_rows,
-        'num_cols': num_cols,
+        'num_rows': game['num_rows'],
+        'num_cols': game['num_cols'],
+        'first_player': game['first_player'],
+        'player_order': game['player_order'],
+        'player': game['player'],
+        'opponent': pieces.kWhite if game['player'] == pieces.kBlack else pieces.kBlack,
+        'player_strategy': game['strategy'],
+        'opponent_strategy': opp_strategy
     }
     
-    cont = 0
     win_prob = 1
+    game_turn = calculate_turn(game_state)
+    log.debug('Game turn: {}'.format(game_turn))
     
     while(True):
         # play the game and examine from the beginning
-        multiBoard_print(game_state['boards'][player_color], game_state['boards'][opp_color], num_rows, num_cols, 'Player - '+player_color, 'Opponent - '+opp_color)
-        p_turn = player_color if cont % 2 == player_order else opp_color 
+        multiBoard_print(game_state['boards'][game_state['player']], game_state['boards'][game_state['opponent']], 
+                         game_state['num_rows'], game_state['num_cols'], 
+                         f'Player - {game_state["player"]}', f'Opponent - {game_state["opponent"]}')
+        p_turn = game_state['player'] if game_turn == game_state['player_order'] else game_state['opponent']
+        
         print(f'It is {p_turn}\'s turn, below are the moves that were examined for lower bound. \n' +
               'Please choose a move to proceed:')
         
         # display the moves and probabilities they were chosen
-        action, prob = display_options(game_state['boards'], player_strategy, opp_strategy, player_color, 0 if p_turn == player_color else 1)
-        print(action, prob)
+        action, prob = display_options(game_state, game_turn)
+        
         # get the next state & display the current win probability in the branch
         game_state, end = play_action(game_state, p_turn, action)
         # print('The win probability for this branch is: {}'.format( ))
-        if end:
-            break
-        
-        cont += 1
+        if not end:
+            # break
+            game_turn = (game_turn + 1) % 2
     
 if __name__ == '__main__':
     main()
