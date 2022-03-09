4014a80a8b516eba49a07d799ebccb76cb50a49d
BedirT <tapkan@ualberta.ca>
2022-03-09 15:09:33 -0700

    optimal pone edit

--- darkhex/algorithms/pone_optimal.py
+++ darkhex/algorithms/pone_optimal.py
@@ -1,248 +1,206 @@
-import copy
+# PONE (Probability One)
+#
+# Finds all probability one wins for the players for the game Dark Hex
+# Assuming both players are playing optimally.
+#
+# Uses pyspiel states to traverse the tree.
+#
+# Uses a dictionary to save the states that are definite win states.
+# These data can be used to early stop when doing search on the tree.
+
 import math
-from itertools import combinations, permutations
-from time import time
-
 import numpy as np
-from utils.pit import pit
-
-from utils.hex import Hex
-
-C_PLAYER1 = 'x'
-C_PLAYER2 = 'o'
-EMPTY_CELL = '.'
-ILLEGAL = 'i'
-TIE = '='
-
-def RES_CHECK(s, h):
-    if CHECK and TO_CHECK_STATE == s and TO_CHECK_H == h:
-        return True
-    return False
+import pyspiel
+import open_spiel.python.algorithms.get_all_states as pyspiel_get_all_states
+from darkhex.utils.cell_state import cellState
 
 
 class PoneOptimal:
-    
-    def __init__(self, board_size):
-        self.num_rows = board_size[0]
-        self.num_cols = board_size[1]
+    def __init__(self, num_rows, num_cols, player=0):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
         self.num_cells = self.num_rows * self.num_cols
 
-        self.state_results = [{} for _ in range(self.num_cells + 1)]
+        self.player = player
+        self.opponent = 1 - player
 
-        self.player = C_PLAYER2  # manually set
-        self.opponent = C_PLAYER1 if self.player == C_PLAYER2 else C_PLAYER2
-        self.find_positions()
+        self.game = pyspiel.load_game(f"dark_hex_ir(num_cols={num_cols},num_rows={num_rows})")
+        
+        self.legal_actions = {}
+        self.legal_states = [{} for _ in range(self.num_cells)]
+        self.empty_states = [set() for _ in range(self.num_cells+1)]  # pyspiel.State
 
+        print("Generating all states...")
+        self.setup_legal_states()   # setup the legal states
+        print(self.empty_states)
 
-    def pone_search(self, state: str, h: int) -> bool:
+    def search(self) -> list:
+        """
+        Find all the legal positions, examine every position
+        in depth for prob 1 wins for players. It fills the dictionary
+        'state results'.
+        """
+        for e in range(self.num_cells + 1):             # empty cells
+            for h in range(math.ceil(self.num_cells / 2)):    # hidden cells
+                if e + h > self.num_cells:
+                    continue
+                legal_states = self.empty_states[e+h]
+                for info_state in legal_states:
+                    res = self.legal_states[h].get(info_state, -2)
+                    if res in [-1, 0, 1]:
+                        res = self.pone_search(info_state, h)
+                        self.legal_states[h][info_state] = res
+        return self.legal_states
+
+    def pone_search(self, info_state: str, h: int) -> bool:
         """
         Recursive algorithm to iterate through sub states and
         determine if the given position is a prob 1 win for player
         or not.
 
         Args:
-            - state:    State to check the legality.
-            - h:        Number of hidden stones.
+            - info_state:   Information state to check.
+            - h:            Number of hidden stones.
         Returns:
-            - True/False  If given state and h is a definite win
+            - True/False    If given info_state and h is a definite win
         """
-        status = self.state_results[h][state]
+        status = self.legal_states[h][info_state]
 
         if status in [self.player, self.opponent]:
             return status
 
-        if self.turn_info(state, h) != self.player:
-            if self.check_state(state, h + 1):
-                return self.pone_search(state, h + 1)
+        if self.turn_info(info_state, h) != self.player:
+            if self.check_state(info_state, h + 1) != -2:
+                return self.pone_search(info_state, h + 1)
+            return -3
         else:
-            vm = [i for i, x in enumerate(state) if x == EMPTY_CELL]
+            legal_actions = self.legal_actions[(info_state, h)]
             if h == 0:
-                for x in vm:
-                    n_state = self.update_state(state, x, self.player, h)
-                    if n_state in self.state_results[h] and self.pone_search(n_state, h):
-                        self.state_results[h][n_state] = self.player
-                        return self.player
+                for action in legal_actions:
+                    n_state = self.update_state(info_state, action, self.player, h)
+                    if n_state in self.legal_states[h]:
+                        if self.pone_search(n_state, h) == self.player:
+                            self.legal_states[h][n_state] = self.player
+                            return self.player
             elif h > 0:
-                for y in vm:
-                    n_state_hW = self.update_state(state, y, self.opponent, h - 1)  # hit the hidden stone
-                    if n_state_hW not in self.state_results[h - 1]:
+                for action in legal_actions:
+                    n_state_hW = self.update_state(
+                        info_state, action, self.opponent, h - 1
+                    )
+                    if n_state_hW not in self.legal_states[h - 1]:
                         continue
-                    n_state_B = self.update_state(state, y, self.player, h)
-                    if (n_state_B in self.state_results[h]
-                        and self.pone_search(n_state_hW, h - 1)
-                        and self.pone_search(n_state_B, h)):
-                        self.state_results[h][n_state_B] = self.player
-                        self.state_results[h - 1][n_state_hW] = self.player
-                        return self.player
-        return False
+                    n_state_B = self.update_state(info_state, action, self.player, h)
+                    if n_state_B in self.legal_states[h]:
+                        if self.pone_search(n_state_hW, h - 1) == self.player:
+                            if self.pone_search(n_state_B, h) == self.player:
+                                self.legal_states[h][n_state_B] = self.player
+                                self.legal_states[h - 1][n_state_hW] = self.player
+                                return self.player
+        return -3
 
-    def update_state(self, state: str, loc: int, player: str, h: int) -> list:
-        """
-        Update the given state, make a move on given location by
-        the given player, and check if the new state is legal.
+    def check_state(self, info_state: str, h: int) -> str:
+        """If the given state is a legal state"""
+        return self.legal_states[h].get(info_state, -2)
 
-        Args:
-            - state:    State to check the legality.
-            - loc:      Location to put the new stone.
-            - player:    The player which will make the move.
-            - h:        Number of hidden stones.
-        Returns:
-            - new_state/[]  New state, if move made to loc by player(player)
-                            is valid, empty list (False) otherwise
-        """
-        new_state = list(copy.deepcopy(state))
-        new_state[loc] = player
+    def turn_info(self, info_state: str, h: int) -> str:
+        """Which players turn is it in the given state"""
+        count_player = self.count(info_state, self.player)
+        count_opp = self.count(info_state, self.opponent) + h
+
+        if self.player == 0 and count_player <= count_opp:
+            return 0
+        elif self.player == 1 and count_opp <= count_player:
+            return 0
+        return 1
+
+    def update_state(self, info_state: str, action: int, player: int, h: int) -> list:
+        """New state after the given action"""
+        new_state = list(info_state)
+        new_state[action] = cellState.kBlack if player == 0 else cellState.kWhite
         new_state = "".join(new_state)
-        if self.check_state(new_state, h):
+        if self.check_state(new_state, h) != -2:
             return new_state
         return ""
 
-    def check_state(self, state: str, h: int) -> str:
+    def setup_legal_states(self) -> None:
         """
-        Checks the state and determines if legal. Updates
-        state_results accordingly if the given state and h
-        is legal.
+        Setup the legal states for each h and e.
+        """
+        all_states = pyspiel_get_all_states.get_all_states(self.game, include_terminals=False, 
+                                                   include_chance_states=False).values()
+        print(f"All States gathered: {len(all_states)}")
+        size_legal_action = 0
+        size_legal_states = 0
+        size_empty_states = 0
+        for state in all_states:
+            info_state = state.information_state_string(self.player)
+            info_state = self.simplify_state(info_state)
+            h = self._num_hidden_stones(state)
+            e = self._num_empty_cells(state)
+            res = -1
+            if (info_state, h) not in self.legal_actions:
+                self.legal_actions[(info_state, h)] = state.legal_actions(self.player)
+                size_legal_action += 1
+            if state.is_terminal():
+                res = self.player if state.returns()[self.player] > 0 else self.opponent
+            if info_state not in self.legal_states[h]:
+                self.legal_states[h][info_state] = res
+                size_legal_states += 1
+            if info_state not in self.empty_states[e]:
+                self.empty_states[e].add(info_state)
+                size_empty_states += 1
+            print(f"Size of legal states: {size_legal_states} | Size of legal actions: {size_legal_action} | Size of empty states: {size_empty_states}", end="\r")
+
+    def _num_hidden_stones(self, state: pyspiel.State) -> int:
+        """
+        Get the number of hidden stones in the given state.
 
         Args:
-            - state:    State to check the legality.
-            - h:        Number of hidden stones.
+            - state:    State to get the number of hidden stones.
         Returns:
-            - res/False Immediate game status for the given state
-                        (Black win - White win - Tie) (B, W, =)
+            - int:      Number of hidden stones.
         """
-        res = self.is_legal(state, h)
-        if res:
-            self.state_results[h][state] = res
-            return res
-        return False
+        player_perspective = self.simplify_state(state.information_state_string(self.player))
+        # count the known opp stones and substract from the real num of opp stones
+        opp_perspective = self.simplify_state(state.information_state_string(self.opponent))
+        opp_stone = cellState.kBlack if self.opponent == 0 else cellState.kWhite
+        num_hidden_stones = 0
+        for p_cell, o_cell in zip(player_perspective, opp_perspective):
+            if o_cell == opp_stone and p_cell != opp_stone:
+                num_hidden_stones += 1
+        return num_hidden_stones
 
-    def turn_info(self, state: str, h: int) -> str:
+    def _num_empty_cells(self, state: pyspiel.State) -> int:
         """
-        Checks which players turn is it given the state and
-        the number of hidden stones.
+        Get the number of empty cells in the given state.
 
         Args:
-            - state:    State to check the legality.
-            - h:        Number of hidden stones.
+            - state:    State to get the number of empty cells.
         Returns:
-            - C_PLAYER1/C_PLAYER2   Player whose turn it is.
+            - int:      Number of empty cells.
         """
-        count_player = state.count(self.player)
-        count_opp = state.count(self.opponent) + h
+        player_perspective = state.information_state_string(self.player)
+        return player_perspective.count('.')
 
-        if self.player == C_PLAYER1 and count_player <= count_opp:
-            return C_PLAYER1
-        elif count_opp <= count_player:
-            return C_PLAYER1
-        return C_PLAYER2
+    @staticmethod
+    def count(info_state: str, player: int) -> int:
+        """Count the player stones in the given info_state"""
+        # count cellState.black_pieces if player == cellState.kBlack
+        ct = 0
+        for c in info_state:
+            if player == 0 and c in cellState.black_pieces:
+                ct += 1
+            elif player == 1 and c in cellState.white_pieces:
+                ct += 1
+        return ct
 
-    def find_positions(self) -> None:
-        """
-        Find all the legal positions, examine every position
-        in depth for prob 1 wins for players. It fills the dictionary
-        'state results'.
-        """
-        for e in pit(range(self.num_cells + 1), color="red"):  # empty cells
-            for h in pit(range(math.ceil(self.num_cells / 2)), color="green"):  # hidden cells
-                if e + h > self.num_cells:
-                    continue
-                states = self.all_states(e + h)
-                print(states)
-                for s in pit(range(len(states)), color="blue"):
-                    state = states[s]
-                    try:
-                        res = self.state_results[h][state]
-                    except:
-                        res = self.is_legal(state, h)
-                    if res:  # if res is legal
-                        self.state_results[h][state] = res
-                        if self.pone_search(state, h):
-                            self.state_results[h][state] = self.player
-
-    def is_legal(self, state: str, h: int) -> str:
-        """
-        Check the given state and determine the legality. If
-        the state is legal examine the immediate result of the
-        state (White/Black(W/B) wins or a tie-(=)).
-
-        Args:
-            - state:    State to check the legality.
-            - h:        Number of hidden stones.
-        Returns:
-            - gs/False  Immediate game status for the given state
-                        (Black win - White win - Tie) (B, W, =)
-        """
-        game = Hex(
-            board_size=[self.num_rows, self.num_cols],
-            board=list(state),
-            legality_check=True,
-            h=h,
-        )
-        info_sets = True
-        if h > 0:
-            # if partial position, check info sets
-            info_sets = self.information_sets(state, h)
-        # Check if the state has odd or even number of
-        # empty cells.
-        e = game.board.count(EMPTY_CELL) - h
-        if (self.num_cells - e) % 2 == 0:
-            # k = 2n
-            game.w_early_w = True  # check for early White win set
-            gs = game.game_status()
-            if gs not in [ILLEGAL, C_PLAYER1] and info_sets:
-                return gs
-        else:
-            # k = 2n + 1
-            game.b_early_w = True  # check for early Black win set
-            gs = game.game_status()
-            if gs not in [ILLEGAL, C_PLAYER2] and info_sets:
-                return gs
-        return False
-
-    def information_sets(self, state: str, h: int) -> bool:
-        """
-        Checks if an information exists for the given position.
-
-        Args:
-            - state:    State to check the existing information sets.
-            - h:        Number of hidden stones on the board.
-        Return:
-        """
-        indexes_empty = [i for i, x in enumerate(state) if x == EMPTY_CELL]
-        comb = combinations(indexes_empty, h)
-        for c in comb:
-            # place the stones on chosen indexes
-            p = [x if i not in c else C_PLAYER2 for i, x in enumerate(state)]
-            # check if legal
-            if self.is_legal(p, 0):
-                return True
-        return False
-
-    def all_states(self, e: int) -> list:
-        """
-        Returns all the board states possible given e, number of
-        empty cells. Fills rest of the board with corresponding
-        Black and White stones, keeping only the ones that are
-        possible legal.
-
-        Args:
-            - e:    Number of empty cells for the board
-                    states to be created.
-
-        Returns:
-            - ls:   List of all possible states for e.
-        """
-        ls = []
-        for num_w in range(self.num_cells // 2 + 1):
-            num_b = self.num_cells - e - num_w
-            if (
-                num_w <= num_b
-                and num_b <= math.ceil(self.num_cells / 2)
-                and not (e + num_b + num_w > self.num_cells)
-            ):
-                for positions in combinations(range(self.num_cells), num_w + num_b):
-                    seq = np.array(["."] * self.num_cells)
-                    seq[list(positions[: num_b + 1])] = self.player
-                    seq[list(positions[num_b + 1 :])] = self.opponent
-                    ls.append("".join(seq))
-        return ls
+    @staticmethod
+    def simplify_state(info_state: str) -> str:
+        info_state = info_state[3:]
+        info_state = info_state.replace("\n", "")
+        for cell in cellState.black_pieces:
+            info_state = info_state.replace(cell, cellState.kBlack)
+        for cell in cellState.white_pieces:
+            info_state = info_state.replace(cell, cellState.kWhite)
+        return info_state
+        
