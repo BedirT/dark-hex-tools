c361cce8d812da6ed8a6de69b1983347953c9fd3
BedirT <tapkan@ualberta.ca>
2022-08-03 16:20:51 -0600

    minor fixes

--- darkhex/experiments/game_size_counter.py
+++ darkhex/experiments/game_size_counter.py
@@ -7,7 +7,7 @@ class CountHistories:
     def __init__(self, game: pyspiel.Game) -> None:
         """Counts the number of histories in a game."""
         state = game.new_initial_state()
-        print("Counting histories...\nNumber of Non Terminal\tNumber of Terminal")
+        print("Counting histories...\nNumber of Non Terminal/Number of Terminal")
         self.num_non_terminal = 0
         self.num_terminal = 0
         self._count_histories(state)
@@ -27,14 +27,15 @@ class CountHistories:
     def report(self) -> None:
         """Prints the number of histories in a game."""
         red, green, end = "\033[91m", "\033[92m", "\033[0m"
-        print(f"{red}{self.num_non_terminal}{end}\t{green}{self.num_terminal}{end}", end="\r")
+        print(f"{red}{self.num_non_terminal}{end}/{green}{self.num_terminal}{end}", end="\r")
 
 def main():
     """Main function."""
-    game = pyspiel.load_game("dark_hex(num_rows=3,num_cols=3)")
+    game = pyspiel.load_game("dark_hex(num_rows=4,num_cols=4)")
     count_histories = CountHistories(game)
     print("Non-terminal histories: {}".format(count_histories.num_non_terminal))
     print("Terminal histories: {}".format(count_histories.num_terminal))
+    print("Total histories: {}".format(count_histories.num_non_terminal + count_histories.num_terminal))
 
 
 if __name__ == "__main__":
