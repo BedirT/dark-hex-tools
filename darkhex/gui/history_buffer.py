4ef6b0f8907cfaa2d1faad0b38e0c04a0c2c9b53
BedirT <tapkan@ualberta.ca>
2022-08-25 12:59:26 -0600

    history buffer updated

--- darkhex/gui/history_buffer.py
+++ darkhex/gui/history_buffer.py
@@ -14,19 +14,23 @@ class gameBuffer:
             "initial_board": initial_board,
         }
         self.info_states = []
-        self.moves_and_boards = []
+        self.actions_and_states = []
         self.board = []
-        self.move_stack = []
+        self.action_stack = []
+        self.action_stack_action_history = []
         self.given_inputs = []
         self.stratgen_class = stratgen_class
         self.add_history_buffer(stratgen_class)
 
     def add_history_buffer(self, stratgen_class, given_input=None):
         self.info_states.append(copy.deepcopy(stratgen_class.info_states))
-        self.moves_and_boards.append(
-            copy.deepcopy(stratgen_class.moves_and_boards))
+        self.actions_and_states.append(
+            copy.deepcopy(stratgen_class.actions_and_states))
         self.board.append(copy.deepcopy(stratgen_class.board))
-        self.move_stack.append(copy.deepcopy(stratgen_class.move_stack))
+        self.action_stack.append(copy.deepcopy(stratgen_class.action_stack))
+        self.action_stack_action_history.append(
+            copy.deepcopy(stratgen_class.action_stack_action_history))
+
         if given_input:
             self.given_inputs.append(given_input)
 
@@ -34,9 +38,10 @@ class gameBuffer:
         """Rewinds the game."""
         if len(self.info_states) > 1:
             self.info_states.pop()
-            self.moves_and_boards.pop()
+            self.actions_and_states.pop()
             self.board.pop()
-            self.move_stack.pop()
+            self.action_stack.pop()
+            self.action_stack_action_history.pop()
             self.given_inputs.pop()
         else:
             return "Cannot rewind anymore.\n"
@@ -46,9 +51,10 @@ class gameBuffer:
     def restart(self) -> str:
         """Restarts the game."""
         self.info_states = self.info_states[:1]
-        self.moves_and_boards = self.moves_and_boards[:1]
+        self.actions_and_states = self.actions_and_states[:1]
         self.board = self.board[:1]
-        self.move_stack = self.move_stack[:1]
+        self.action_stack = self.action_stack[:1]
+        self.action_stack_action_history = self.action_stack_action_history[:1]
         self.given_inputs = []
         self._update_game(0)
         return "Restarted the game.\n"
@@ -62,27 +68,27 @@ class gameBuffer:
 
     def _revert(self, idx):
         self.info_states = self.info_states[:idx + 1]
-        self.moves_and_boards = self.moves_and_boards[:idx + 1]
+        self.actions_and_states = self.actions_and_states[:idx + 1]
         self.board = self.board[:idx + 1]
-        self.move_stack = self.move_stack[:idx + 1]
+        self.action_stack = self.action_stack[:idx + 1]
+        self.action_stack_action_history = self.action_stack_action_history[:idx
+                                                                            + 1]
         self.given_inputs = self.given_inputs[:idx + 1]
         self._update_game(idx)
 
     def _update_game(self, idx):
         self.stratgen_class.info_states = copy.deepcopy(self.info_states[idx])
-        self.stratgen_class.moves_and_boards = copy.deepcopy(
-            self.moves_and_boards[idx])
+        self.stratgen_class.actions_and_states = copy.deepcopy(
+            self.actions_and_states[idx])
         self.stratgen_class.board = copy.deepcopy(self.board[idx])
-        self.stratgen_class.move_stack = copy.deepcopy(self.move_stack[idx])
+        self.stratgen_class.action_stack = copy.deepcopy(self.action_stack[idx])
+        self.stratgen_class.action_stack_action_history = copy.deepcopy(
+            self.action_stack_action_history[idx])
 
     def revert_to_state(self, idx=None, state=None):
         if idx and state:
             raise ValueError("Cannot use both idx and state.")
-        if idx:
-            self._revert(idx)
-        elif state:
-            idx = self.search_history(state)
-            if idx != -1:
-                self._revert(idx)
-            else:
-                raise ValueError("State not found.")
+        idx = idx if idx else self.search_history(state)
+        if idx == -1:
+            raise ValueError("State not found.")
+        self._revert(idx)
